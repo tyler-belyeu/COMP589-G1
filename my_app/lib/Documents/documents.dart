06deaa3d80105e67e45603a55d2738852fef39b9
tbelyeu <tylerdbelyeu@gmail.com>
2023-02-23 14:55:30 -0800

    app skeleton built

---
+++ my_app/lib/Documents/documents.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Docs extends StatelessWidget {
+  const Docs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Documents Page'),
+    );
+  }
+}
---
+++ my_app/lib/nav.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+import 'package:my_app/Chat/chat.dart';
+import 'package:my_app/Documents/documents.dart';
+import 'package:my_app/Links/links.dart';
+
+class Nav extends StatefulWidget {
+  const Nav({super.key});
+
+  @override
+  State<Nav> createState() => _NavState();
+}
+
+class _NavState extends State<Nav> {
+  int _selectedIndex = 0;
+  final List<Widget> _widgetOptions = <Widget>[
+    const Chat(),
+    const Docs(),
+    const Links()
+  ];
+
+  void _onItemTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+          child: GNav(
+            backgroundColor: Colors.black,
+            color: Colors.white,
+            activeColor: Colors.white,
+            tabBackgroundColor: Colors.grey.shade800,
+            gap: 8,
+            padding: const EdgeInsets.all(16),
+            tabs: const [
+              GButton(
+                icon: Icons.chat,
+                text: "Chat",
+              ),
+              GButton(
+                icon: Icons.assignment,
+                text: "Docs",
+              ),
+              GButton(
+                icon: Icons.link,
+                text: "Links",
+              )
+            ],
+            selectedIndex: _selectedIndex,
+            onTabChange: _onItemTap,
+          ),
+        ),
+      ),
+    );
+  }
+}
